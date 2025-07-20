03636347e71832a32085309c8977243e2f8d37b5
priyanshu pundir <priyanshupundir345@gmail.com>
2025-07-20 20:48:00 +0530

    arrays and loop started

---
+++ scripts/11-to-do-list.js
@@ -0,0 +1,12 @@
+const todolist = [];
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    todolist.push(name);
+    console.log(todolist);
+
+    inputElement.value = ''; //reset to the text box
+
+    
+}
