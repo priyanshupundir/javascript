c59cf2358b76b990ab9a6687795a08da46a130d7
priyanshu pundir <priyanshupundir345@gmail.com>
2025-07-20 22:03:04 +0530

    lesson 11 completed

--- scripts/11-to-do-list.js
+++ scripts/11-to-do-list.js
@@ -1,12 +1,27 @@
-const todolist = [];
+const todolist = ['make dinner', 'wash dishes'];
+rendertodoList();
 
+function rendertodoList(){
+
+
+    let todoListHTML = '';
+
+    for(let i = 0; i < todolist.length; i++){
+        const todo = todolist[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
+    }
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
 function addTodo() {
+
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
     todolist.push(name);
     console.log(todolist);
 
     inputElement.value = ''; //reset to the text box
-
+    rendertodoList();
     
 }
